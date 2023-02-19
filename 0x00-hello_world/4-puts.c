d3448f595df2f4cd42a3f3bb05254135e9a65bf9
Gideon5348 <gideonmutai096@gmail.com>
2023-02-19 02:00:27 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-
+/**
+ * A program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
