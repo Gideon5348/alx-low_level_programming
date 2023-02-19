5449e322bf010f371f14c92a85cda34e636b723c
Gideon5348 <gideonmutai096@gmail.com>
2023-02-19 02:30:14 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* A programs thats prints  printf function */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
