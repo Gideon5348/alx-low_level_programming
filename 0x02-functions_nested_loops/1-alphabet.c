c5c8f97702a4928bfee72c5029caf8b062208f64
Gideon5348 <gideonmutai096@gmail.com>
2023-02-22 12:32:23 -0800

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet- prints the alphabet, in lowercase.
+ */
+void print_alphabet(void)
+{
+	char ll;
+
+	for (ll = 'a'; ll <= 'z', ll++)
+		_putchar(ll);
+
+	_putchar('\n');
+}
