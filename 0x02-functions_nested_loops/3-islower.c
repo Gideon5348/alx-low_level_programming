6befa94e86f3e9b23f993f67fb6255793b6760a2
Gideon5348 <gideonmutai096@gmail.com>
2023-02-22 13:43:48 -0800

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - check if char is lowercase
  *@c: is the char to be checked
- * Return : 1 if char is lowercase, otherwise 0.
+ * Return: 1 if char is lowercase, otherwise 0.
  *
  */
 
