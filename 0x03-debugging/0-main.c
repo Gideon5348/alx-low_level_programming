41c0937009855d7c177b932bccf4c1b26228b4a2
Gideon5348 <gideonmutai096@gmail.com>
2023-02-23 05:59:04 -0800

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - test if the correct output when given a case of 0
+ * Return: 0
+ */
+
+positive_or_negative(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
