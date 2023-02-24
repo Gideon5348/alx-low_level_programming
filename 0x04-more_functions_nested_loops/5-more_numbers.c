d3f0a4b2f6fe7295e799cb3f99f8315e9f004bf9
Gideon5348 <gideonmutai096@gmail.com>
2023-02-24 04:09:00 -0800

    6. The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -25,9 +25,9 @@ void more_numbers(void)
 		{
 		if (i >= 10)
 		{
-		_putchar(i / 10 + '0');
+		_putchar(i/10 + '0');
 		}
-	_putchar(i % 10 + '0');
+	_putchar(i%10 + '0');
 		}
 	_putchar('\n');
 	}
