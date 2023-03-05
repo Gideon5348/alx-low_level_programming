3e5ba465ca40d219a3d0ad1d1fabbe224921e957
Gideon5348 <gideonmutai096@gmail.com>
2023-03-05 05:21:01 -0800

    7. Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *);
-char *leet(char *);
+char *leet(char *n);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
