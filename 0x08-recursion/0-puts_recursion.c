ffdff3c46e233155e050c88a416d7a2d11b52a91
Gideon5348 <gideonmutai096@gmail.com>
2023-03-10 13:57:06 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+0x08. C - Recursion
+This folder contains my ALX C-Recursion programming projects
