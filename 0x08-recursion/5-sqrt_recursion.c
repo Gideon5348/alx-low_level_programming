2d73c4bb8ca5b22bd5c5124c616e21ab38dc0448
Gideon5348 <gideonmutai096@gmail.com>
2023-06-16 10:10:03 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int sqrt_helper(int n, int s, int e);
+
 /**
  * _sqrt_recursion -  returns the natural square root of a number.
  * @n: number to calculate square root of.
@@ -8,9 +10,6 @@
  * If n does not have a natural square root, the function should return -1
  */
 
-int _sqrt_recursion(int n);
-int sqrt_helper(int n, int guess);
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -18,24 +17,29 @@ int _sqrt_recursion(int n)
 	else if (n == 0 || n == 1)
 		return (n);
 	else
-		return (sqrt_helper(n, n / 2));
+		return (sqrt_helper(n, 1, n));
 }
 
 /**
  * sqrt_helper - recursive helper function to calculate square root.
- * @n: The number to calculate the square root of
- * @guess: The current guess for the square root
+ * @n: The number to calculate the square root of.
+ * @s: The starting point for the search.
+ * @e: The ending point for the search.
  *
  * Return: the natural square root of a number.
  *  If n does not have a natural square root, the function should return -1
  */
 
-int sqrt_helper(int n, int guess)
+int sqrt_helper(int n, int s, int e)
 {
-	if (guess * guess == n)
-		return (guess);
-	else if (guess == 0 || guess == 1)
+	int mid = (s + e) / 2;
+
+	if (mid * mid == n)
+		return (mid);
+	else if (s >= e)
 		return (-1);
+	else if (mid * mid > n)
+		return (sqrt_helper(n, s, mid - 1));
 	else
-		return (sqrt_helper(n, guess - 1));
+		return (sqrt_helper(n, mid + 1, e));
 }
