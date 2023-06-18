f9734c78db9a12968681ed226e21d4000d8eea7b
Gideon5348 <gideonmutai096@gmail.com>
2023-06-18 02:11:56 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a digit (0-9)
+ * @c: the character to check
+ *
+ * Return: 1 if the character is a digit, 0 otherwise
+ */
+
+int _isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int y = 0;
+	int sign = 1;
+	int outcome = 0;
+
+	if (s[0] == '-' || s[0] == '+')
+	{
+		if (s[0] == '-')
+			sign = -1;
+
+		y++;
+	}
+
+	while (s[y])
+	{
+		if (!_isdigit(s[y]))
+			break;
+		outcome = outcome * 10 + (s[y] - '0');
+		y++;
+	}
+
+	return (outcome * sign);
+}
 
 /**
  * main - Prints the name of the program
@@ -14,9 +59,11 @@ int main(int argc, char *argv[])
 	int num1, num2, outcome;
 
 	if (argc != 3)
+	{
 		printf("Error\n");
 
-	return (1);
+		return (1);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
