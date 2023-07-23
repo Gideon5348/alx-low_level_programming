693bc88dbfe78b8e8efdede5c1b4f43d62a4a97d
Gideon5348 <gideonmutai096@gmail.com>
2023-07-23 13:59:49 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,73 +7,124 @@
 
 #define BUF_SIZE 1024
 
+/* Function prototypes */
+void display_usage_and_exit(const char *program_name);
+void display_read_error_and_exit(const char *file_name);
+void display_write_error_and_exit(const char *file_name);
+void display_close_error_and_exit(int fd);
+
 /**
- * main - Copies the content of one file to another.
- * @argc: The number of arguments.
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success, or an error code on failure.
+ * Return: 0 on success, otherwise exits with an error code.
  */
-
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUF_SIZE];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	struct stat st;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		display_usage_and_exit(argv[0]);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		display_read_error_and_exit(argv[1]);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
+		display_write_error_and_exit(argv[2]);
 	}
 
-	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
+	do {
+		bytes_read = read(fd_from, buffer, BUF_SIZE);
+		if (bytes_read == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
+			display_read_error_and_exit(argv[1]);
 		}
+
+		if (bytes_read > 0)
+		{
+			bytes_written = write(fd_to, buffer, bytes_read);
+			if (bytes_written == -1 || bytes_written != bytes_read)
+			{
+				display_write_error_and_exit(argv[2]);
+			}
+		}
+	} while (bytes_read > 0);
+
+	if (fstat(fd_from, &st) == -1)
+	{
+		display_read_error_and_exit(argv[1]);
 	}
 
-	if (bytes_read == -1)
+	if (fchmod(fd_to, st.st_mode) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
+		display_write_error_and_exit(argv[2]);
 	}
 
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
+		display_close_error_and_exit(fd_from);
 	}
 
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		display_close_error_and_exit(fd_to);
 	}
 
 	return (0);
+}
+
+/**
+ * display_usage_and_exit - Displays usage message and exits with code 97.
+ * @program_name: The name of the program.
+ */
+
+void display_usage_and_exit(const char *program_name)
+{
+	dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", program_name);
+	exit(97);
+}
+
+/**
+ * display_read_error_and_exit - Displays read error and exits with code 98.
+ * @file_name: The name of the file that encountered the read error.
+ */
+
+void display_read_error_and_exit(const char *file_name)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
+	exit(98);
+}
+
+/**
+ * display_write_error_and_exit - Displays write error and exits with code 99.
+ * @file_name: The name of the file that encountered the write error.
+ */
+
+void display_write_error_and_exit(const char *file_name)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+	exit(99);
+}
+
+/**
+ * display_close_error_and_exit - Displays close error and exits with code 100.
+ * @fd: The file descriptor that encountered the close error.
+ */
+
+void display_close_error_and_exit(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
