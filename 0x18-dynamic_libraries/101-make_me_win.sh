2ec6cbda63b3d3ed757f019df4edea0003661749
Gideon5348 <gideonmutai096@gmail.com>
2023-08-27 14:35:59 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-gcc -shared -o libtest.so -fPIC test.c
-export LD_PRELOAD=./libtest.so:$LD_LIBRARY_PATH
+gcc -shared -o myprintf.so -fPIC test.c
+export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
