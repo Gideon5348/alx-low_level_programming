54bc82649424eabb92309cbb9c47c53952e57c48
Gideon5348 <gideonmutai096@gmail.com>
2023-09-08 16:00:37 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL;
-	unsigned long int i = 0;
+	hash_table_t *new_table;
+	unsigned long int i;
 
 	/* Allocate memory for the hash table structure */
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-	return (NULL);
+		return (NULL);
 
 	/* Allocate memory for the array of pointers to hash nodes */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
@@ -27,10 +27,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	/* Initialize the elements of the array to NULL */
-	for (i = 0; i < size; i++)
-	new_table->array[i] = NULL;
-
 	new_table->size = size;
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
