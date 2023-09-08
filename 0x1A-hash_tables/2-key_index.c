494717480cb15690f9d24bee1319938e91eb8aef
Gideon5348 <gideonmutai096@gmail.com>
2023-09-08 14:14:13 -0700

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  *
  * Return: The index at which the key/value pair should be stored
  */
- 
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value;
