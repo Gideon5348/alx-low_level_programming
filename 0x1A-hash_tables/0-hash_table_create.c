a10794acc3e0077783f56b7f3db2e56327a3b2c4
Gideon5348 <gideonmutai096@gmail.com>
2023-09-09 04:18:06 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,12 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
-	/* Allocate memory for the entire hash table structure */
 	new_table = malloc(sizeof(hash_table_t) + sizeof(hash_node_t *) * size);
 	if (new_table == NULL)
 		return (NULL);
 
-	/* Initialize the size and array elements to NULL */
 	new_table->size = size;
 	new_table->array = (hash_node_t **)((char *)new_table + sizeof(hash_table_t));
 	for (i = 0; i < size; i++)
