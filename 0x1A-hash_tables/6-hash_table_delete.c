6fcdeee6f864758efb78f0ceaed1629d3fe4742e
Gideon5348 <gideonmutai096@gmail.com>
2023-09-09 03:29:48 -0700

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,22 +10,23 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *current, *tmp;
 
 	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-	current = ht->array[i];
-	while (current != NULL)
-	{
-		tmp = current;
-		current = current->next;
-		free(tmp->key);
-		free(tmp->value);
-		free(tmp);
-	}
+		hash_node_t *current = ht->array[i];
+		hash_node_t *next;
+
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
 	}
 
 	free(ht->array);
